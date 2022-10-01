9866698908f023a6bcd11d0748174d182d751155
Mikey Lemmon <mikey@thrivestone.com>
2022-10-01 13:39:17 +1300

    Add react-native-web View (causes hydration error)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,29 +1,25 @@
+import { View } from "react-native-web";
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
+      <View />
+      <p>
+        See console logs for hydration error caused by inclusion of RNW View
+        component: (
+        <span
+          style={{ color: "red" }}
+        >{`"Warning: Expected server HTML to contain a matching <meta> in <head>.`}</span>
+        )
+      </p>
       <ul>
         <li>
           <a
             target="_blank"
-            href="https://remix.run/tutorials/blog"
+            href="https://github.com/remix-run/remix/discussions/1578"
             rel="noreferrer"
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+            Remix + RNW Github Discussion
           </a>
         </li>
       </ul>
